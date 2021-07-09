a11c5377296b40d2a14aedf22b8efc214b5f52e6
it kimopay <it.kimopay@gmail.com>
2021-07-09 13:42:24 +0800

    Setting Structure

--- app/src/main/java/id/co/quizapp/MainActivity.kt
+++ app/src/main/java/id/co/quizapp/MainActivity.kt
@@ -2,10 +2,21 @@ package id.co.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import id.co.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dataBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        dataBinding.bottomNavigationView.setupWithNavController(Navigation.findNavController(this, R.id.navHost))
+
     }
 }
